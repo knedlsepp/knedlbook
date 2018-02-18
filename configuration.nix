6ee3d91e22b01df16d7886ec81fd9a5123ee18a6
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-02-18 23:06:30 +0100

    Make vim the default editor

--- configuration.nix
+++ configuration.nix
@@ -39,6 +39,7 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
+  programs.vim.defaultEditor = true;
   programs.bash.enableCompletion = true;
   programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
